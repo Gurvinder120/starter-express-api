6342bb05e51cd9cf70d575a61e5ce918d62b0318
Gurvinder120 <145794996+Gurvinder120@users.noreply.github.com>
2023-10-31 00:42:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+const cors = require("cors");
+app.use(cors())
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
